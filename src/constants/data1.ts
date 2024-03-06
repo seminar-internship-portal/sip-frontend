6c8bf8a020ed535d9996d9dbd50ea36e31655d79
worldtourmydream@gmail.com <worldtourmydream@gmail.com>
2024-03-06 22:57:51 +0530

    Table creation

--- src/constants/data1.ts
+++ src/constants/data1.ts
@@ -1,22 +1,34 @@
-type Payment = {
-  id: string;
-  amount: number;
-  status: "pending" | "processing" | "success" | "failed";
-  email: string;
-};
+// data.ts (getData)
+import { Student } from "@/app/Mentor/dashboard/columns";
 
-export const payments: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "489e1d42",
-    amount: 125,
-    status: "processing",
-    email: "example@gmail.com",
-  },
-  // ...
-];
+function getData(year: string): Promise<Student[]> {
+  const Baseurl = process.env.API_BASE_URL;
+  return fetch(`https://sip-backend-api.onrender.com/api/v1/student`, {
+    next: { revalidate: 10 },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return response.json();
+    })
+    .then((resdata) => {
+      const data = resdata.data;
+
+      const students = data.map((item: any) => ({
+        username: item.username,
+        email: item.email,
+        fullName: item.fullName,
+        mobileNo: item.mobileNo,
+        rollNo: item.rollNo,
+        prnNo: item.prnNo,
+        registrationId: item.registrationId,
+        id: item.id,
+      }));
+
+      console.log(students);
+      return students;
+    });
+}
+
+export default getData;
