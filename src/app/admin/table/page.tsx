5f5f183ff00e531cf14d7a08d8841c65498990dc
Shivam <shivamchandak9@gmail.com>
2024-03-08 00:32:44 +0530

    added table data

--- src/app/admin/table/page.tsx
+++ src/app/admin/table/page.tsx
@@ -1,33 +1,45 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import { Student, columns } from "./columns";
 import { DataTable } from "./data-table";
-import { useState, useEffect } from "react";
 
-function getData(): Promise<Student[]> {
-  // Fetch data from your API here.
-  return Promise.resolve([
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]);
-}
-
-export default function DemoPage() {
-  const [data, setData] = useState<Student[] | null>(null);
+export default function StudentTable() {
+  const [data, setData] = useState<Student[]>([]);
+  const [ayear, setAyear] = useState("");
 
   useEffect(() => {
-    getData().then((result) => {
-      setData(result);
-    });
-  }, []);
+    async function fetchData() {
+      console.log(ayear);
+      const response = await fetch(
+        `https://sip-backend-api.onrender.com/api/v1/student?year=${ayear}`
+      ); // Assuming the API route is defined in pages/api/v1/student.ts
+      const resData = await response.json();
+
+      if (!resData.success) {
+        throw new Error("Failed to fetch data");
+      }
+
+      setData(
+        resData.data.map((item: any) => ({
+          username: item.username,
+          email: item.email,
+          fullName: item.fullName,
+          mobileNo: item.mobileNo,
+          rollNo: item.rollNo,
+          prnNo: item.prnNo,
+          registrationId: item.registrationId,
+          id: item.id,
+        }))
+      );
+    }
+
+    fetchData();
+  }, [ayear]); // Fetch data whenever ayear changes
 
   return (
-    <div className="container mx-auto py-10">
-      {data ? <DataTable columns={columns} data={data} /> : <p>Loading...</p>}
+    <div className="container mx-auto py-10 overflow-y-auto">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
