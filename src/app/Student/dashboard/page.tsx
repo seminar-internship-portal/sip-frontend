eb8b5fc975463cc7a3d529aedd7ed45e7ba2a1dd
Saif Shaikh <103292777+saifshaikhgithub@users.noreply.github.com>
2024-03-29 21:31:31 +0530

    Update page.tsx
    Dashboard left

--- src/app/Student/dashboard/page.tsx
+++ src/app/Student/dashboard/page.tsx
@@ -1,44 +1,60 @@
-// page.tsx
+//page.tsx
+"use client";
+import React from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { getCookie } from "cookies-next";
 
-import { Student, columns } from "./columns";
-import { DataTable } from "./data-table";
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+);
 
-async function getData(): Promise<Student[]> {
-  const response = await fetch(
-    "https://sip-backend-api.onrender.com/api/v1/student",
-    {
-      next: { revalidate: 10 },
-    }
-  );
-  const resdata = await response.json();
+const page = () => {
+  const getdata = async () => {
+    try {
+      const studentCookie = getCookie("Student");
+      if (!studentCookie) {
+        console.error("Student cookie not found");
+        return;
+      }
+      console.log(studentCookie);
 
-  const data = resdata.data;
-
-  if (!resdata.success) {
-    throw new Error("Failed to fetch data");
-  }
-
-  const students: Student[] = data.map((item: any) => ({
-    username: item.username,
-    email: item.email,
-    fullName: item.fullName,
-    mobileNo: item.mobileNo,
-    rollNo: item.rollNo,
-    prnNo: item.prnNo,
-    registrationId: item.registrationId,
-    id: item.id,
-  }));
-
-  // console.log(students);
-  return students;
-}
-
-export default async function StudentTable() {
-  const data = await getData();
-
+      const { accessToken } = JSON.parse(studentCookie);
+      console.log(accessToken);
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+        credentials: "include",
+      };
+      // Update profile in the backend API
+      const response = await fetch(
+        "https://sip-backend-api.onrender.com/api/v1/student/updateAccountDetails",
+        {
+          method: "POST",
+          headers: headers,
+        }
+      );
+      console.log(response);
+    } catch (error) {}
+  };
   return (
-    <div className="container mx-auto py-10 overflow-y-auto">
-      <DataTable columns={columns} data={data} />
+    <div className="flex justify-center content-center items-center px-10 m-16x p-14">
+      <ScrollArea className="h-96 w-96 rounded-md border">
+        <div className="p-4">
+          <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+          {tags.map((tag) => (
+            <>
+              <div key={tag} className="text-sm">
+                {tag}
+              </div>
+              <Separator className="my-2" />
+            </>
+          ))}
+        </div>
+      </ScrollArea>
+      <button onClick={getdata}>Hello</button>
     </div>
   );
-}
+};
+
+export default page;
